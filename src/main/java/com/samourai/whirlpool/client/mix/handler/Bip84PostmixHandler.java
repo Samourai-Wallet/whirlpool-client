658a99495bf5c1977310c4ab2b1ce9ada913e5c9
zeroleak <e17ba2c64b67f25a1cddb9bd80f4c53a2df7e9b3>
2019-01-22 21:42:39 +0100

    add Bip84PostmixHandler

---
+++ src/main/java/com/samourai/whirlpool/client/mix/handler/Bip84PostmixHandler.java
@@ -0,0 +1,39 @@
+package com.samourai.whirlpool.client.mix.handler;
+
+import com.samourai.wallet.client.Bip84Wallet;
+import com.samourai.wallet.hd.HD_Address;
+import com.samourai.wallet.segwit.bech32.Bech32UtilGeneric;
+import org.bitcoinj.core.ECKey;
+import org.bitcoinj.core.NetworkParameters;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Bip84PostmixHandler implements IPostmixHandler {
+  private static final Logger log = LoggerFactory.getLogger(Bip84PostmixHandler.class);
+  private Bech32UtilGeneric bech32Util = Bech32UtilGeneric.getInstance();
+  private Bip84Wallet postmixWallet;
+  private HD_Address receiveAddress;
+
+  public Bip84PostmixHandler(Bip84Wallet postmixWallet) {
+    this.postmixWallet = postmixWallet;
+    this.receiveAddress = null;
+  }
+
+  @Override
+  public synchronized String computeReceiveAddress(NetworkParameters params) throws Exception {
+    this.receiveAddress = postmixWallet.getNextAddress();
+
+    String bech32Address = bech32Util.toBech32(receiveAddress, params);
+    if (log.isDebugEnabled()) {
+      log.debug(
+          "receiveAddress=" + bech32Address + ", path=" + receiveAddress.toJSON().get("path"));
+    }
+    return bech32Address;
+  }
+
+  @Override
+  public IPremixHandler computeNextPremixHandler(UtxoWithBalance receiveUtxo) {
+    ECKey receiveKey = receiveAddress.getECKey();
+    return new PremixHandler(receiveUtxo, receiveKey);
+  }
+}
