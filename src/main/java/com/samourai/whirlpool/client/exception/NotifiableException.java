d3f7a5fc1a40fb9e074fa4588d6412532c9553fd
zeroleak <e17ba2c64b67f25a1cddb9bd80f4c53a2df7e9b3>
2019-01-29 12:17:19 +0100

    add NotifiableException.status

--- src/main/java/com/samourai/whirlpool/client/exception/NotifiableException.java
+++ src/main/java/com/samourai/whirlpool/client/exception/NotifiableException.java
@@ -1,8 +1,37 @@
 package com.samourai.whirlpool.client.exception;
 
+import org.glassfish.grizzly.http.util.HttpStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class NotifiableException extends Exception {
+  private static final Logger log = LoggerFactory.getLogger(NotifiableException.class);
+  private static final int STATUS_DEFAULT = HttpStatus.INTERNAL_SERVER_ERROR_500.getStatusCode();
+
+  private int status;
 
   public NotifiableException(String message) {
+    this(message, STATUS_DEFAULT);
+  }
+
+  public NotifiableException(HttpStatus status) {
+    this(status.getReasonPhrase(), status.getStatusCode());
+  }
+
+  public NotifiableException(String message, int status) {
     super(message);
+    this.status = status;
+  }
+
+  public int getStatus() {
+    return status;
+  }
+
+  public static NotifiableException computeNotifiableException(Exception e) {
+    if (NotifiableException.class.isAssignableFrom(e.getClass())) {
+      return (NotifiableException) e;
+    }
+    log.warn("Exception obfuscated to user", e);
+    return new NotifiableException("Error");
   }
 }
